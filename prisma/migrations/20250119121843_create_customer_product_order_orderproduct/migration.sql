32c797c5ed8799c4c01259c17e1863dd6a681cc8
Felipe Leal <lealfelipealves@gmail.com>
2025-01-19 09:22:21 -0300

    feat: create entities migration

---
+++ prisma/migrations/20250119121843_create_customer_product_order_orderproduct/migration.sql
@@ -0,0 +1,63 @@
+-- CreateEnum
+CREATE TYPE "ProductCategory" AS ENUM ('SNACK', 'SIDE_DISH', 'DRINK', 'DESSERT');
+
+-- CreateEnum
+CREATE TYPE "OrderStatus" AS ENUM ('RECEIVED', 'IN_PREPARATION', 'READY', 'FINALIZED');
+
+-- CreateTable
+CREATE TABLE "customers" (
+    "id" TEXT NOT NULL,
+    "cpf" TEXT,
+    "name" TEXT,
+    "email" TEXT,
+    "isAnonymous" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "customers_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "products" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL DEFAULT 0,
+    "category" "ProductCategory" NOT NULL DEFAULT 'SNACK',
+    "orderId" TEXT,
+
+    CONSTRAINT "products_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "orders" (
+    "id" TEXT NOT NULL,
+    "customer_id" TEXT NOT NULL,
+    "status" "OrderStatus" NOT NULL DEFAULT 'RECEIVED',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+
+    CONSTRAINT "orders_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "order_products" (
+    "id" TEXT NOT NULL,
+    "order_id" TEXT,
+    "product_id" TEXT,
+
+    CONSTRAINT "order_products_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "customers_email_key" ON "customers"("email");
+
+-- AddForeignKey
+ALTER TABLE "products" ADD CONSTRAINT "products_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "orders"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "orders" ADD CONSTRAINT "orders_customer_id_fkey" FOREIGN KEY ("customer_id") REFERENCES "customers"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "order_products" ADD CONSTRAINT "order_products_order_id_fkey" FOREIGN KEY ("order_id") REFERENCES "orders"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "order_products" ADD CONSTRAINT "order_products_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "products"("id") ON DELETE SET NULL ON UPDATE CASCADE;
