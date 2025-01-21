003d67b878ce7a5710d273e7afae0978e56bf2e6
Felipe Leal <lealfelipealves@gmail.com>
2025-01-20 21:43:30 -0300

    docs: attachment drawio event storming

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build()
   const documentFactory = () => SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup('api', app, documentFactory)
+  SwaggerModule.setup('docs', app, documentFactory)
 
   await app.listen(port)
 }
